0b0bad0a402171d9fcc4e8b85f10f68dd59811b6
bvalentic <bobvalentic@quickenloans.com>
2018-11-16 15:14:45 -0500

    First commit!

---
+++ main.js
@@ -0,0 +1,35 @@
+function WrongAnswer1() {
+    alert("Wrong!\nTry again, pipsqueak!");
+}
+
+function CorrectAnswer1() {
+    alert("Correct!\nOh yeah!");
+    document.location.href = "question2.html";
+}
+
+function WrongAnswer2() {
+    alert("Wrong!\nTry again, sucker!");
+}
+
+function CorrectAnswer2() {
+    alert("Correct!\nHell yeah, brother!");
+    document.location.href = "question3.html";
+}
+
+function WrongAnswer3() {
+    alert("Wrong!\nTry again, fool!")
+}
+
+function CorrectAnswer3() {
+    alert("Correct!\nRight on, baby!")
+    document.location.href = "question4.html";
+}
+
+function WrongAnswer4() {
+    alert("Wrong!\nTry again, turkey!")
+}
+
+function CorrectAnswer4() {
+    alert("Correct!\nNice moves, dude!");
+    document.location.href = "victory.html";
+}
